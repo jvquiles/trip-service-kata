d5855bd451752de0fe55a3a09280f05ae4d881d4
juquiles@aidacanarias.com <juquiles@aidacanarias.com>
2023-06-05 16:09:37 +0200

    test: fail when user is not logged

--- c#/tests/TripServiceKata.Tests/TripServiceShould.cs
+++ c#/tests/TripServiceKata.Tests/TripServiceShould.cs
@@ -1,6 +1,26 @@
-﻿namespace TripServiceKata.Tests
+﻿using System;
+using FluentAssertions;
+using NSubstitute;
+using TripServiceKata.Entity;
+using TripServiceKata.Exception;
+using TripServiceKata.Service;
+using Xunit;
+
+namespace TripServiceKata.Tests
 {
     public class TripServiceShould
     {
+        [Fact]
+        public void FailWhenThereIsNoLoggedUser()
+        {
+            var userSession = Substitute.For<IUserSession>();
+            var tripDao = Substitute.For<ITripDAO>();
+            var tripService = new TripService(userSession, tripDao);
+            var user = new User();
+
+            var action = new Action(() => tripService.GetTripsByUser(user));
+
+            action.Should().Throw<UserNotLoggedInException>();
+        }
     }
 }
